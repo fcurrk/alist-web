597167009240a1454a17495b7dd11777a22f7b9b
fcurrk <48197714+fcurrk@users.noreply.github.com>
2023-01-03 15:12:46 +0800

    update

--- src/pages/home/header/Header.tsx
+++ src/pages/home/header/Header.tsx
@@ -2,21 +2,29 @@ import {
   HStack,
   useColorModeValue,
   Image,
+  Heading,
+  IconButton,
   Center,
   Icon,
   Kbd,
+  Text,
 } from "@hope-ui/solid"
 import { Show } from "solid-js"
-import { getSetting, objStore, State } from "~/store"
-import { BsSearch } from "solid-icons/bs"
+import { useT } from "~/hooks";
+import { getSetting, layout, objStore, setLayout, State, getMainColor } from "~/store"
+import { BsGridFill, BsSearch } from "solid-icons/bs"
+import { FaSolidListUl } from "solid-icons/fa"
 import { CenterLoading } from "~/components"
 import { Container } from "../Container"
 import { bus } from "~/utils"
 import { Layout } from "./layout"
 
 export const Header = () => {
+  const t = useT()
   const logos = getSetting("logo").split("\n")
   const logo = useColorModeValue(logos[0], logos.pop())
+  const logotexts = getSetting("logo_text").split("\n")
+  const logotext = useColorModeValue(logotexts[0], logotexts.pop())
   return (
     <Center
       class="header"
@@ -31,12 +39,11 @@ export const Header = () => {
           justifyContent="space-between"
         >
           <HStack class="header-left" h="44px">
-            <Image
-              src={logo()!}
-              h="$full"
-              w="auto"
-              fallback={<CenterLoading />}
-            />
+          {getSetting("logo") ? (
+             <Image src={logo()!} h="$full" w="auto" fallback={<CenterLoading />}/>
+          ) : (
+             <Heading size="lg" >{logotext}</Heading>
+          )}
           </HStack>
           <HStack class="header-right" spacing="$2">
             <Show when={objStore.state === State.Folder}>
