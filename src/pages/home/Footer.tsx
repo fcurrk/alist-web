9028aeed4eeb02fc93c504aea46b27cf337fc2f1
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-07-02 09:00:13 +0800

    Update 3.35.0.0702

--- src/pages/home/Footer.tsx
+++ src/pages/home/Footer.tsx
@@ -1,8 +1,8 @@
-import { Anchor, HStack, VStack } from "@hope-ui/solid"
+import { Anchor, HStack, VStack, Text } from "@hope-ui/solid"
 import { Link } from "@solidjs/router"
 import { AnchorWithBase } from "~/components"
 import { useT } from "~/hooks"
-import { me } from "~/store"
+import { getSetting, me } from "~/store"
 import { UserMethods } from "~/types"
 
 export const Footer = () => {
@@ -10,16 +10,24 @@ export const Footer = () => {
   return (
     <VStack class="footer" w="$full" py="$4">
       <HStack spacing="$1">
-        <Anchor href="https://github.com/alist-org/alist" external>
-          {t("home.footer.powered_by")}
-        </Anchor>
-        <span>|</span>
+        <Text className="line1">
+          {t("©2023 ")} {getSetting("site_title")}
+        </Text>
+      </HStack>
+      <HStack spacing="$1">
+        <Text className="line1">{t("home.footer.powered_by")}</Text>
+        <span> |</span>
         <AnchorWithBase
           as={Link}
           href={UserMethods.is_guest(me()) ? "/@login" : "/@manage"}
         >
           {t(UserMethods.is_guest(me()) ? "login.login" : "home.footer.manage")}
         </AnchorWithBase>
+        {getSetting("site_beian") && (
+          <Anchor href="https://beian.miit.gov.cn" external>
+            {getSetting("site_beian")}
+          </Anchor>
+        )}
       </HStack>
     </VStack>
   )
